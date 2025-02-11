e41b68511959d04048447b10abeaf43d1511b2b5
hammah12 <hm8917@gmail.com>
2025-02-11 13:46:00 -0600

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,8 +1,8 @@
 // src/supabaseClient.js
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
-const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
+const supabaseUrl = 'https://tsdqcubdaswmhiwskufu.supabase.co' ;
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRzZHFjdWJkYXN3bWhpd3NrdWZ1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzkyNDg2NzIsImV4cCI6MjA1NDgyNDY3Mn0.O1MDsy3B82m15qP-xnoNI2Rtw_VmhKnxXbE7hsvMW-Q' ;
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
