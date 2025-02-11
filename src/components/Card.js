89687abc454dfe6937c1032cdc5fd5006e70ad5c
hammah12 <hm8917@gmail.com>
2025-02-11 16:43:27 -0600

    Update Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,15 @@
 // src/components/Card.js
 import React from 'react';
 
-const Card = ({ children, className }) => {
-  return <div className={`bg-white shadow-md rounded-lg overflow-hidden ${className}`}>{children}</div>;
+const Card = ({ children, className, image }) => {
+  return (
+    <div className={`bg-white shadow-lg rounded-lg overflow-hidden transform hover:scale-105 transition-all duration-300 ${className}`}>
+      {image && <img src={image} alt="Restaurant" className="w-full h-40 object-cover" />}
+      <div className="p-4">
+        {children}
+      </div>
+    </div>
+  );
 };
 
 export default Card;
