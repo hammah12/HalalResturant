2df0ddf2b78d752fd84959d2deed622b6245e865
hammah12 <hm8917@gmail.com>
2025-02-10 22:57:41 -0600

    Create supabaseClient.js

---
+++ src/supabaseClient.js
@@ -0,0 +1,5 @@
+// src/supabaseClient.js
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://tsdqcubdaswmhiwskufu.supabase.co'; 
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRzZHFjdWJkYXN3bWhpd3NrdWZ1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzkyNDg2NzIsImV4cCI6MjA1NDgyNDY3Mn0.O1MDsy3B82m15qP-xnoNI2Rtw_VmhKnxXbE7hsvMW-Q'; 
