47f506af47e2e5de26a19c900696a8b148ad2041
hammah12 <hm8917@gmail.com>
2025-02-11 16:08:09 -0600

    Create Card.js

---
+++ src/components/Card.js
@@ -0,0 +1,8 @@
+// src/components/Card.js
+import React from 'react';
+
+const Card = ({ children, className }) => {
+  return <div className={`bg-white shadow-md rounded-lg overflow-hidden ${className}`}>{children}</div>;
+};
+
+export default Card;
