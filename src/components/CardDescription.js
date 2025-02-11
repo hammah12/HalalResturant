83d5e12e05cdd3e42f17f6923b5e99708a81e943
hammah12 <hm8917@gmail.com>
2025-02-11 16:04:30 -0600

    Delete src/components/CardDescription.js

--- src/components/CardDescription.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const CardDescription = ({ children, className }) => {
-  return <p className={`card-description ${className}`}>{children}</p>;
-};
-
-export default CardDescription;
